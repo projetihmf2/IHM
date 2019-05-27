48008a0ff306bcb93ab9621307ff0d953e0f7dfe
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-05-27 10:01:45 +0200

    Add files via upload

--- SelectionFiche.java
+++ SelectionFiche.java
@@ -1,4 +1,8 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -11,40 +15,49 @@ class SelectionFiche extends JPanel implements ActionListener {
     private FichierGenealogique genealogique;
 
     private ArrayList<JButton> tabButtons;
+    private Font font;
 
-    public SelectionFiche() 
-    {
-    	this.tabButtons = new ArrayList<JButton> ();
+    public SelectionFiche() {
+
         this.setLayout(new FlowLayout());
+        this.setBackground(Color.GRAY);
+        this.setPreferredSize(new Dimension(125, 10));
+        this.font = new Font("Courier", Font.PLAIN, 14);
+        this.setFont(this.font);
 
-        this.add(new JButton("Jean"));
+        this.tabButtons = new ArrayList<JButton>();
 
     }
 
     public void setGenealogique(FichierGenealogique genealogique) {
-    	System.out.println("recu");
+        System.out.println("reçu");
         this.genealogique = genealogique;
         this.maj();
+        System.out.println("MAj terminer");
     }
-    
+
     public FichierGenealogique getGenealogique() {
-    	return this.genealogique ;
+        return this.genealogique;
     }
 
     public void maj() {
 
-        this.removeAll();
+        for (JButton but : this.tabButtons) {
+            this.remove(but);
+        }
         this.tabButtons.clear();
 
         for (FicheGenealogique f : genealogique.getListeFiches()) {
-            this.tabButtons.add(new JButton(f.getNom()));
+
+            this.tabButtons.add(new JButton(String.format("%-10s", f.getNom())));
         }
 
         for (JButton but : this.tabButtons) {
             this.add(but);
+            but.setFont(this.font);
             but.addActionListener(this);
         }
-
+        this.updateUI();
     }
 
     @Override
