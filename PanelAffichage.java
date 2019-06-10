1fbf0b51a5a05d8516bbdc61eda8dafaa12e4b82
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-06-11 00:42:52 +0200

    Add files via upload

--- PanelAffichage.java
+++ PanelAffichage.java
@@ -9,52 +9,94 @@ class PanelAffichage extends JPanel {
     private Rectangle rectangleSelection;
     private int nbNiveau;
 
+    // test
+    private ArrayList<int[]> listePos;
+    private ArrayList<FicheGenealogique> listeFiche;
+
     public PanelAffichage() {
         this.setVisible(true);
     }
 
     public void paint(Graphics g) {
         if (this.fiche != null) {
+            // effacement de la fenetre
             g.clearRect(0, 0, this.getWidth(), this.getHeight());
-            int nvHauteur = this.nbNiveau;
-            int nbLongueurLigMax = (int) Math.pow(2, nbNiveau - 1);
-            int longueur = getWidth() / (nbLongueurLigMax + 1);
-            int largeur = getHeight() / (nvHauteur * 2);
 
-            for (int i = 1; i <= nvHauteur; i++) {
+            // Calcul de nombre d'ancetre maximum
+            int nbLongueurLigMax = (int) Math.pow(2, nbNiveau - 1);
+
+            // Reglage de la longeur et de la largeur en fonction du nombre de composant
+            Rectangle.setLongeur(this.getWidth() / (nbLongueurLigMax + 1));
+            Rectangle.setLargeur(this.getHeight() / (this.nbNiveau * 2));
+            Rectangle.setDecalage(50);
+
+            // Cree un liste qui contient tout les rectangles
+            ArrayList<Rectangle> allRectangle = new ArrayList<Rectangle>();
+
+            int longueur = Rectangle.getLongeur();
+            int largeur = Rectangle.getLargeur();
+            // parcour de l'ensemble des niveaux
+            for (int i = 1; i <= this.nbNiveau; i++) {
+                // creation d'un variable k qui assure la bonne possition des rectanges
+                int k = 0;
                 for (int j = 1; j <= (int) Math.pow(2, i); j = j + 2) {
-                    g.drawRect((int) (j * (getWidth() / Math.pow(2, i))) - longueur / 2,
-                            (int) (nvHauteur + 1 - i) * getHeight() / (nvHauteur + 1) - (largeur / 2), longueur,
-                            largeur); // (int
-                                      // x,
-                                      // int
-                                      // y,
-                                      // int
-                                      // width,
-                                      // int
-                                      // height)
-                    if (i != nvHauteur) {
-                        g.drawLine((int) (j * (getWidth() / Math.pow(2, i))),
-                                (int) (nvHauteur + 1 - i) * getHeight() / (nvHauteur + 1) - (largeur / 2),
-                                (int) ((j + 2 * ((j - 1) / 2)) * (getWidth() / Math.pow(2, i + 1))),
-                                (int) (nvHauteur + 1 - (i + 1)) * getHeight() / (nvHauteur + 1) + (largeur / 2));
-                        g.drawLine((int) (j * (getWidth() / Math.pow(2, i))),
-                                (int) (nvHauteur + 1 - i) * getHeight() / (nvHauteur + 1) - (largeur / 2),
-                                (int) ((2 * j + 1) * (getWidth() / Math.pow(2, i + 1))),
-                                (int) (nvHauteur + 1 - (i + 1)) * getHeight() / (nvHauteur + 1) + (largeur / 2));
+                    k++;
+                    FicheGenealogique ficheCourante = estPresent(i, k);
+
+                    allRectangle.add(new Rectangle((int) (j * (getWidth() / Math.pow(2, i))) - longueur / 2,
+                            (int) (this.nbNiveau + 1 - i) * getHeight() / (this.nbNiveau + 1) - (largeur / 2),
+                            ficheCourante));
+
+                    if (ficheCourante != null) {
+
+                        if (ficheCourante.getPere() != null)
+                            g.drawLine((int) (j * (getWidth() / Math.pow(2, i))),
+                                    (int) (this.nbNiveau + 1 - i) * getHeight() / (this.nbNiveau + 1) - (largeur / 2),
+                                    (int) ((j + 2 * ((j - 1) / 2)) * (getWidth() / Math.pow(2, i + 1))),
+                                    (int) (this.nbNiveau + 1 - (i + 1)) * getHeight() / (this.nbNiveau + 1)
+                                            + (largeur / 2));
+                        if (ficheCourante.getMere() != null)
+                            g.drawLine((int) (j * (getWidth() / Math.pow(2, i))),
+                                    (int) (this.nbNiveau + 1 - i) * getHeight() / (this.nbNiveau + 1) - (largeur / 2),
+                                    (int) ((2 * j + 1) * (getWidth() / Math.pow(2, i + 1))),
+                                    (int) (this.nbNiveau + 1 - (i + 1)) * getHeight() / (this.nbNiveau + 1)
+                                            + (largeur / 2));
                     }
                 }
+            }
+            for (Rectangle r : allRectangle) {
+                r.peindre(g);
             }
         }
     }
 
+    // Creation de l'arbre lors de l'envoi d'un fiche
     public void nouveau(FicheGenealogique fiche) {
         this.fiche = fiche;
         this.nbNiveau = compter(fiche);
+        // creation des listes
+        this.listeFiche = new ArrayList<FicheGenealogique>();
+        this.listePos = new ArrayList<int[]>();
+        creeListPos(fiche, 1, 1);
+
         this.repaint();
 
     }
 
+    // regarde si il y a une fiche a cette endroit
+    private FicheGenealogique estPresent(int hauteur, int position) {
+
+        for (int[] i : this.listePos) {
+            // si il existe cette position dans la liste
+            if (i[0] == hauteur && i[1] == position)
+                // retourne la fiche avec cette position
+                return this.listeFiche.get(this.listePos.indexOf(i));
+        }
+
+        return null;
+    }
+
+    // compte le nombre de niveau max de l'arbre
     private int compter(FicheGenealogique f) {
         int nombre1 = 1;
         int nombre2 = 1;
@@ -67,4 +109,24 @@ class PanelAffichage extends JPanel {
         return (nombre1 > nombre2 ? nombre1 : nombre2);
 
     }
+
+    private void creeListPos(FicheGenealogique f, int hauteur, int position) {
+        if (f == null)
+            return;
+
+        int[] a = { hauteur, position };
+        // ajout du tableau de position
+        this.listePos.add(a);
+        // ajout de la liste de fiche
+        this.listeFiche.add(f);
+
+        if (f.getPere() != null) {
+            creeListPos(f.getPere(), hauteur + 1, position * 2 - 1);
+
+        }
+        if (f.getMere() != null) {
+            creeListPos(f.getMere(), hauteur + 1, position * 2);
+        }
+    }
+
 }
--- Rectangle.java
+++ Rectangle.java
@@ -4,28 +4,52 @@ import java.awt.Graphics;
 
 import javax.swing.*;
 
-public class Rectangle
-{
+public class Rectangle {
     private int posX;
     private int posY;
-    private static int largeur = 125;
-    private static int longeur = 80;
-    private static final int decalage = 25;
+    private static int largeur;
+    private static int longeur;
+    private static int decalage;
     private FicheGenealogique fiche;
 
-    public Rectangle(int posX, int posY, FicheGenealogique fiche)
-    {
+    public Rectangle(int posX, int posY, FicheGenealogique fiche) {
         this.posX = posX;
         this.posY = posY;
         this.fiche = fiche;
     }
 
-    public void paint(Graphics g)
-    {
-        g.setColor(Color.BLACK);
-        g.drawRect(this.posX, this.posY, Rectangle.longeur, Rectangle.largeur);
-        g.drawString("Arthur  Baradel", posX + Rectangle.decalage, posY + Rectangle.decalage);
+    public void peindre(Graphics g) {
+        if (this.fiche != null) {
 
-        System.out.println("TEST");
+            g.setColor(Color.BLACK);
+            g.drawRect(this.posX, this.posY, Rectangle.longeur, Rectangle.largeur);
+            g.drawString(fiche.getNom() + "\\" + fiche.getPrenom(), posX + Rectangle.decalage,
+                    posY + Rectangle.decalage);
+        }
+
+    }
+
+    public static void setLargeur(int largeur) {
+        Rectangle.largeur = largeur;
+    }
+
+    public static void setLongeur(int longeur) {
+        Rectangle.longeur = longeur;
+    }
+
+    public static void setDecalage(int decalage) {
+        Rectangle.decalage = decalage;
+    }
+
+    public static int getLargeur() {
+        return Rectangle.largeur;
+    }
+
+    public static int getLongeur() {
+        return Rectangle.longeur;
+    }
+
+    public static int getDecalage() {
+        return Rectangle.decalage;
     }
 }
